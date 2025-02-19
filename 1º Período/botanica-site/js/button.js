947c901161fdfd9d183bfb382b3f44f7ac7c196a
Gabriel Gomes <65258908+srfirew@users.noreply.github.com>
2025-02-19 02:47:38 -0300

    Add files via upload

---
+++ 1º Período/botanica-site/js/button.js
@@ -0,0 +1,52 @@
+//Projetos
+
+//Home
+
+const home = document.getElementById("home");
+
+home.addEventListener("click", () => {
+
+    window.location.href = "/index.html";
+
+    
+
+})
+
+//Registro
+
+const registro = document.getElementById("conectar");
+
+registro.addEventListener("click", () => {
+
+    window.location.href = "/conectar.html";
+
+})
+
+//Pesquisas
+
+const pesquisa = document.getElementById("pesquisa");
+
+pesquisa.addEventListener("click", () => {
+
+    window.location.href = "/pesquisas.html";
+
+})
+
+//Logar
+
+document.addEventListener("DOMContentLoaded", function () {
+    const formLogin = document.getElementById("f-login");
+
+    if (formLogin) {
+        formLogin.addEventListener("submit", function (event) {
+            event.preventDefault();
+            window.location.href = "/pesquisador.html";
+        });
+    }
+});
+
+//Cadastro planta
+
+//Cadastro árvore
+
+//Cadastro projeto
