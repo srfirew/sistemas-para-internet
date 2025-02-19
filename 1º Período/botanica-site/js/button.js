a7401c0269e847fab4133114eee37d74f862e549
Gabriel Gomes <gg5850523@gmail.com>
2025-02-19 03:01:28 -0300

    aaa

--- 1º Período/botanica-site/js/button.js
+++ 1º Período/botanica-site/js/button.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (formLogin) {
         formLogin.addEventListener("submit", function (event) {
             event.preventDefault();
-            window.location.href = "pesquisador.html";
+            window.location.href = "https://srfirew.github.io/sistemas-para-internet/1%C2%BA%20Per%C3%ADodo/botanica-site/pesquisador.html";
         });
     }
 });
